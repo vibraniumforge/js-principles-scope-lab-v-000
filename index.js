5669c37c6d5ed8d57792cefdd60fa053a67a6230
Greg Morgan <gregm9999@gmail.com>
2019-01-08 22:13:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 var customerName="bob";
 
 function upperCaseCustomerName(customerName) {
-  return customerName.tuUpperCase();
+  return customerName.toUpperCase();
 }
 
 function setBestCustomer () {
